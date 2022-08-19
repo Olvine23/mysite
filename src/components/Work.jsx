05327400db4b77e48919cf3cf97fe4fc84a5d4bd
Olvine23 <olivinegeorge@gmail.com>
2022-08-19 17:40:21 +0300

    decreased some padding

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,109 +1,116 @@
-import React from 'react';
+import React from "react";
 
-import 'tw-elements'
+import "tw-elements";
 
 const Work = () => {
   return (
-    <div name='work' className='w-full h-screen text-gray-300 bg-[#0a192f]'>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#28c7fa]'>
+    <div name="work" className="w-full h-full text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-3 flex flex-col justify-center w-full h-full">
+        <div className="">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#28c7fa]">
             Work
           </p>
-          <p className='py-6'> Check out some of my recent work</p>
+          <p className="py-6"> Check out some of my recent work</p>
         </div>
 
-{/* Container */}
-        <div className='  sm:grid-cols-2 ' >
+        {/* Container */}
+        <div className=" ">
+          {/* Grid Item */}
+          <div
+            id="carouselDarkVariant"
+            class="carousel slide carousel-fade carousel-dark relative"
+            data-bs-ride="carousel"
+          >
+            <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0  ">
+              <button
+                data-bs-target="#carouselDarkVariant"
+                data-bs-slide-to="0"
+                class="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                data-bs-target="#carouselDarkVariant"
+                data-bs-slide-to="1"
+                aria-label="Slide 1"
+              ></button>
+              <button
+                data-bs-target="#carouselDarkVariant"
+                data-bs-slide-to="2"
+                aria-label="Slide 1"
+              ></button>
+            </div>
 
-            {/* Grid Item */}
-            <div
-  id="carouselDarkVariant"
-  class="carousel slide carousel-fade carousel-dark relative"
-  data-bs-ride="carousel"
->
- 
-  <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-    <button
-      data-bs-target="#carouselDarkVariant"
-      data-bs-slide-to="0"
-      class="active"
-      aria-current="true"
-      aria-label="Slide 1"
-    ></button>
-    <button
-      data-bs-target="#carouselDarkVariant"
-      data-bs-slide-to="1"
-      aria-label="Slide 1"
-    ></button>
-    <button
-      data-bs-target="#carouselDarkVariant"
-      data-bs-slide-to="2"
-      aria-label="Slide 1"
-    ></button>
-  </div>
+            <div class="carousel-inner relative w-full h-800 overflow-hidden">
+              <div class="carousel-item active relative float-left  w-full">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp"
+                  class="block  w-full"
+                  alt="Motorbike Smoke"
+                />
+                <div class="carousel-caption hidden md:block absolute text-center">
+                  <h5 class="text-xl">First slide label</h5>
+                  <p>
+                    Some representative placeholder content for the first slide.
+                  </p>
+                </div>
+              </div>
 
-   
-  <div class="carousel-inner relative w-full overflow-hidden">
-    
-    <div class="carousel-item active relative float-left w-full">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(19).webp"
-        class="block w-full"
-        alt="Motorbike Smoke"
-      />
-      <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
+              <div class="carousel-item relative float-left w-full">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(35).webp"
+                  class="block w-full"
+                  alt="Mountaintop"
+                />
+                <div class="carousel-caption hidden md:block absolute text-center">
+                  <h5 class="text-xl">Second slide label</h5>
+                  <p>
+                    Some representative placeholder content for the second
+                    slide.
+                  </p>
+                </div>
+              </div>
 
-     
-    <div class="carousel-item relative float-left w-full">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(35).webp"
-        class="block w-full"
-        alt="Mountaintop"
-      />
-      <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
+              <div class="carousel-item relative h-90 float-left w-full">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp"
+                  class="block w-full"
+                  alt="Woman Reading a Book"
+                />
+                <div class="carousel-caption hidden md:block absolute text-center">
+                  <h5 class="text-xl">Third slide label</h5>
+                  <p>
+                    Some representative placeholder content for the third slide.
+                  </p>
+                </div>
+              </div>
+            </div>
 
- 
-    <div class="carousel-item relative float-left w-full">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(40).webp"
-        class="block w-full"
-        alt="Woman Reading a Book"
-      />
-      <div class="carousel-caption hidden md:block absolute text-center">
-        <h5 class="text-xl">Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-   
-  <button
-    class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-    type="button"
-    data-bs-target="#carouselDarkVariant"
-    data-bs-slide="prev"
-  >
-    <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button
-    class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-    type="button"
-    data-bs-target="#carouselDarkVariant"
-    data-bs-slide="next"
-  >
-    <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+            <button
+              class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+              type="button"
+              data-bs-target="#carouselDarkVariant"
+              data-bs-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon inline-block bg-no-repeat"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+              class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+              type="button"
+              data-bs-target="#carouselDarkVariant"
+              data-bs-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon inline-block bg-no-repeat"
+                aria-hidden="true"
+              ></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
         </div>
       </div>
     </div>
